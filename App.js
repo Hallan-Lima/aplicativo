a89dfb7cd2e144d4b577aee5707c44bf7e740d6b
Hallan-Lima <hallansrl.t@gmail.com>
2024-02-15 15:33:23 -0300

    implementando funcionalidades simples para testar

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+  const handlePress = () => {
+    // Adicione o código que você deseja executar quando o botão for pressionado
+    console.log('Botão pressionado!');
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ola mundo!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Clique aqui</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +25,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#007BFF', // Cor do botão
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff', // Cor do texto do botão
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
